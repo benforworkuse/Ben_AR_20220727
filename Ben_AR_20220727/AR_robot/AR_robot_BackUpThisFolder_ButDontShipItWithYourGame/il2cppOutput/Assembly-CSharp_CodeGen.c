16bb17f6d20c073a327fcda51d75203bfdb60ab8
benforworkuse <105775320+benforworkuse@users.noreply.github.com>
2022-07-27 16:32:19 +0800

    Update_更新雙按鈕_20220727

---
+++ Ben_AR_20220727/AR_robot/AR_robot_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Ben.CardManager::Awake()
+extern void CardManager_Awake_mA3A8CA25A7C6B2A582A75421021F0AC104309E7A (void);
+// 0x00000002 System.Void Ben.CardManager::OnJumpPressed(Vuforia.VirtualButtonBehaviour)
+extern void CardManager_OnJumpPressed_m13BA79228F65687625D9EDF0EAD4D5878030142F (void);
+// 0x00000003 System.Void Ben.CardManager::CardFound()
+extern void CardManager_CardFound_m1005F028A8C587EC58EBFA7EC6F4F413A94FE5FA (void);
+// 0x00000004 System.Void Ben.CardManager::CardLost()
+extern void CardManager_CardLost_m44DA2BE810D99A1F349E5BA788C4AB82F91722E6 (void);
+// 0x00000005 System.Void Ben.CardManager::Attack()
+extern void CardManager_Attack_m8A46E95588E038FA530C6F37CBD11A096A8714B5 (void);
+// 0x00000006 System.Void Ben.CardManager::.ctor()
+extern void CardManager__ctor_m4C8B5F49BF37C17254034553BD4FEF1D9A8633F6 (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CardManager_Awake_mA3A8CA25A7C6B2A582A75421021F0AC104309E7A,
+	CardManager_OnJumpPressed_m13BA79228F65687625D9EDF0EAD4D5878030142F,
+	CardManager_CardFound_m1005F028A8C587EC58EBFA7EC6F4F413A94FE5FA,
+	CardManager_CardLost_m44DA2BE810D99A1F349E5BA788C4AB82F91722E6,
+	CardManager_Attack_m8A46E95588E038FA530C6F37CBD11A096A8714B5,
+	CardManager__ctor_m4C8B5F49BF37C17254034553BD4FEF1D9A8633F6,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	5045,
+	4003,
+	5045,
+	5045,
+	5045,
+	5045,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
--- Ben_AR_20220727/Assets/Scripts/CardManager.cs
+++ Ben_AR_20220727/Assets/Scripts/CardManager.cs
@@ -1,24 +1,51 @@
 
+
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
 
 namespace Ben
 {
     public class CardManager : MonoBehaviour
     {
         [SerializeField, Header("KID 圖片")] private DefaultObserverEventHandler observerKID;
+        [SerializeField, Header("機器人ani")] private Animator anirobot;
+        [SerializeField, Header("攻擊按鈕")] private Button btnAttack;
+        [SerializeField, Header("虛擬跳躍按鈕")] private VirtualButtonBehaviour vbbjump;
+
+        private string shootaction = "Shoot";
+        private AudioSource audBGM;
 
         private void Awake()
         {
             observerKID.OnTargetFound.AddListener(CardFound);
             observerKID.OnTargetLost.AddListener(CardLost);
+
+            btnAttack.onClick.AddListener(Attack);
+            vbbjump.RegisterOnButtonReleased(OnJumpPressed);
+            audBGM = GameObject.Find("BGM").GetComponent<AudioSource>();
         }
+
+        private void OnJumpPressed(VirtualButtonBehaviour obj)
+        {
+            print("<color=#3366dd>跳躍!!!</color>");
+           
+        }
+
         private void CardFound()
         {
             print("<color=yellow>找到卡片</color>");
+            anirobot.SetTrigger(shootaction);
+            audBGM.Play();
         }
         private void CardLost()
         {
             print("<color=red>卡片辨識失敗</color>");
+            audBGM.Stop();
+        }
+        private void Attack()
+        {
+            print("<color=#987655>攻擊!!!</color>");
         }
     }
 }
